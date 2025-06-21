72bc532457df9502c835e91b6f621c9350fcafee
Your Name <your_email@example.com>
2025-06-21 15:27:49 +0200

    nav styling

--- agreeable-asteroid/src/lib/sanityClient.js
+++ agreeable-asteroid/src/lib/sanityClient.js
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client';
 
 export const client = sanityClient({
-  projectId: 'jaagz5m2', // replace with your real project ID if different
+  projectId: 'jaagz5m2', //
   dataset: 'production',
   useCdn: true,
   apiVersion: '2023-01-01',
